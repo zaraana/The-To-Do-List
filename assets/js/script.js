cf4df39d2f106a9d294659b64431026684b6b8fa
zaraana <zara.zara92@outlook.be>
2024-05-04 13:37:23 +0200

    update

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,17 +10,21 @@ addTask.addEventListener("click", function(){
 
     if(taskText !== ""){
         const listItem = document.createElement("li");
-        listItem.textContent = taskText;
-        inputBox.value = "";
-
+        
         const checkbox = document.createElement("input");
         checkbox.type = "checkbox";
         listItem.appendChild(checkbox);
+        
+        const p = document.createElement("p");
+        p.textContent = taskText;
+        listItem.appendChild(p);
+        inputBox.value = "";
 
-        const deleteButton = document.createElement("button");
-        deleteButton.type = "button";
+        const deleteButton = document.createElement("i");
+        //deleteButton.type = "button";
+        deleteButton.classList.add('fas', 'fa-trash');
         // deleteButton.className = "delete-button";
-        deleteButton.innerHTML = '<i class="fad fa-trash"></i>';
+        //deleteButton.innerHTML = '<i class="fa-solid fa-trash"></i>';
         listItem.appendChild(deleteButton);
 
         deleteButton.addEventListener("click", function(){
@@ -31,9 +35,10 @@ addTask.addEventListener("click", function(){
 
         checkbox.addEventListener("click", function() {
             if (checkbox.checked) {
-                listItem.classList.add("completed");
+                p.classList.add("completed");
+
             } else {
-                listItem.classList.remove("completed");
+                p.classList.remove("completed");
             }
             });
             
@@ -45,38 +50,38 @@ addTask.addEventListener("click", function(){
 });
 
 
-// const filterSelect = document.getElementById("filter-tasks");
+const filterSelect = document.getElementById("filter-tasks");
 
-//     filterSelect.addEventListener("change", function() {
-//     const selectedOption = this.value;
+    filterSelect.addEventListener("change", function() {
+    const selectedOption = this.value;
 
-//     taskList.querySelectorAll("li").forEach(function(task) {
-//         if (selectedOption === "all") {
-//         task.classList.remove("hidden");
-//         } else if (selectedOption === "completed" && task.querySelector("input[type='checkbox']").checked) {
-//         task.classList.remove("hidden");
-//         } else if (selectedOption === "not-complete" && !task.querySelector("input[type='checkbox']").checked) {
-//         task.classList.remove("hidden");
-//         } else {
-//         task.classList.add("hidden");
-//         }
-//     });
-//     });
+    taskList.querySelectorAll("li").forEach(function(task) {
+        if (selectedOption === "all") {
+        task.classList.remove("hidden");
+        } else if (selectedOption === "completed" && task.querySelector("input[type='checkbox']").checked) {
+        task.classList.remove("hidden");
+        } else if (selectedOption === "not-completed" && !task.querySelector("input[type='checkbox']").checked) {
+        task.classList.remove("hidden");
+        } else {
+        task.classList.add("hidden");
+        }
+    });
+    });
 
 
-        // const taskInput = document.querySelector("#input-box");
-        // const addButton = document.querySelector("button");
+        const taskInput = document.getElementById("input-box");
+        const addButton = document.querySelector("button");
         
-        // addButton.addEventListener("click", function() {
-        // const taskList = JSON.parse(localStorage.getItem("tasks")) || [];
-        // const newTask = {
-        //     text: taskInput.innerHTML
-        //     completed: false;
-        // };
-        // taskList.push(newTask);
-        // localStorage.setItem("tasks", JSON.stringify(taskList));
-        // // clear the input field
-        // taskInput.value = "";
-        // });
+        addButton.addEventListener("click", function() {
+        console.log(taskInput.value);
+        const newTask = {
+            text: taskInput.value,
+        };
+        const taskList = JSON.parse(localStorage.getItem("tasks")) || [];
+        taskList.push(newTask);
+        localStorage.setItem("tasks", JSON.stringify(taskList));
+        // clear the input field
+        taskInput.value = "";
+        });
 
 
